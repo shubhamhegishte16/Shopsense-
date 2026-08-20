916db4218435144d52b07bfa6eef690d6079633a
shubhamhegishte16-create <shubhamhegishte16@gmail.com>
2026-08-20 22:21:37 +0530

    Backend cors issue resolved

--- Backend/index.js
+++ Backend/index.js
@@ -21,6 +21,8 @@ app.use(
       "http://localhost:5173",
       "http://localhost:3000",
       "http://127.0.0.1:5173",
+      "https://shopsense-ashen.vercel.app",
+      process.env.FRONTEND_URL
     ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
